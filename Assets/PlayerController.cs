d8dc1c5bd2d634a3e5180e9dfb88c95f91cfed71
pzawadzki <admin@machinacyfrowa.pl>
2023-03-13 15:58:39 +0100

    killing zombies and dying

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    float hp = 10;
     public GameObject bulletPrefab;
     public float bulletSpeed = 20;
     public float playerSpeed = 2;
     Vector2 movementVector;
     Transform bulletSpawn;
+    public GameObject hpBar;
+    Scrollbar hpScrollBar;
 
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
         bulletSpawn = transform.Find("BulletSpawn");
+        hpScrollBar = hpBar.GetComponent<Scrollbar>();
     }
 
     // Update is called once per frame
@@ -41,5 +46,26 @@ public class PlayerController : MonoBehaviour
         bullet.transform.parent = null;
         bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed, ForceMode.VelocityChange);
         Destroy(bullet, 5);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+   
+            hp--;
+            if(hp <= 0) Die();
+            hpScrollBar.size = hp / 10;
+            Vector3 pushVector = collision.gameObject.transform.position - transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(pushVector.normalized*5, ForceMode.Impulse);
+        }
+    }
+    void Die()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        transform.Translate(Vector3.up);
+        transform.Rotate(Vector3.right * -90);
+        
+        //Time.timeScale = 0;
     }
 }
--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ZombieBehaviour : MonoBehaviour
 {
+    int hp = 10;
+
     GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -14,9 +16,28 @@ public class ZombieBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform.position);
-        //Vector3 playerDirection = transform.position - player.transform.position;
+        if(hp> 0)
+        {
+            transform.LookAt(player.transform.position);
+            //Vector3 playerDirection = transform.position - player.transform.position;
+
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
         
-        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            hp--;
+            if(hp <= 0)
+            {
+                transform.Translate(Vector3.up);
+                transform.Rotate(Vector3.right * -90);
+                GetComponent<BoxCollider>().enabled = false;
+                Destroy(transform.gameObject, 10);
+            }
+        }
     }
 }
