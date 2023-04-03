237f4dc6565f3cee3573a42c10a61f68cae6509a
pzawadzki <admin@machinacyfrowa.pl>
2023-04-03 15:00:14 +0200

    navmesh enabled zombies

--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieBehaviour : MonoBehaviour
 {
     int hp = 10;
 
     GameObject player;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -18,10 +22,16 @@ public class ZombieBehaviour : MonoBehaviour
     {
         if(hp> 0)
         {
-            transform.LookAt(player.transform.position);
+            //transform.LookAt(player.transform.position);
             //Vector3 playerDirection = transform.position - player.transform.position;
 
-            transform.Translate(Vector3.forward * Time.deltaTime);
+            //transform.Translate(Vector3.forward * Time.deltaTime);
+
+            agent.destination = player.transform.position;
+        }
+        else
+        {
+            agent.speed = 0;
         }
         
     }
